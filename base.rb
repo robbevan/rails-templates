164efbab7667537516b32c4412f920d7c4b3b8e0
Rob Bevan <robbevan@xpt.com>
2009-06-30 14:44:09 +0100

    added basic (base) app template

---
+++ base.rb
@@ -0,0 +1,37 @@
+# remove tmp dirs
+run "rmdir tmp/{ pids, sessions, sockets, cache }"
+ 
+# remove unnecessary stuff
+run "rm README log/*.log public/index.html public/images/rails.png"
+ 
+# keep empty dirs
+run("find . \\( -type d -empty \\) -and \\( -not -regex ./\\.git.* \\) -exec touch {}/.gitignore \\;")
+ 
+# init git repo
+git :init
+ 
+# basic .gitignore file
+file '.gitignore', 
+%q{log/*.log
+log/*.pid
+db/*.db
+db/*.sqlite3
+db/schema.rb
+tmp/**/*
+.DS_Store
+doc/api
+doc/app
+config/database.yml
+autotest_result.html
+coverage
+public/javascripts/*_[0-9]*.js
+public/stylesheets/*_[0-9]*.css
+public/attachments
+}
+ 
+# copy sample database config
+run "cp config/database.yml config/database.yml.sample"
+
+# commit changes
+git :add => "."
+git :commit => "-a -m 'Initial Rails app'"
