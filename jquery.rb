f815582bda749deb109886f7f64d17c68cdc1f83
Rob Bevan <robbevan@xpt.com>
2009-07-01 17:32:16 +0100

    formatting changes

--- jquery.rb
+++ jquery.rb
@@ -1,7 +1,6 @@
 git :rm => "public/javascripts/*"
 
-file 'public/javascripts/jquery.js', 
-  open('http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js').read
+file 'public/javascripts/jquery.js', open('http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js').read
 
 file "public/javascripts/application.js", <<-JS
 $(function() {
