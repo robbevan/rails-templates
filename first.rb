b478848b3024ec0c8b86389bb7f1c5e842f308e9
Rob Bevan <robbevan@xpt.com>
2009-07-01 17:03:10 +0100

    renamed default > first

--- first.rb
+++ first.rb
@@ -7,16 +7,12 @@ run "rm README log/*.log public/index.html public/images/rails.png"
 # keep empty dirs
 run("find . \\( -type d -empty \\) -and \\( -not -regex ./\\.git.* \\) -exec touch {}/.gitignore \\;")
  
-# init git repo
-git :init
- 
 # basic .gitignore file
 file '.gitignore', 
 %q{log/*.log
 log/*.pid
 db/*.db
 db/*.sqlite3
-db/schema.rb
 tmp/**/*
 .DS_Store
 doc/api
@@ -31,7 +27,3 @@ public/attachments
  
 # copy sample database config
 run "cp config/database.yml config/database.yml.sample"
-
-# commit changes
-git :add => "."
-git :commit => "-a -m 'Initial Rails app'"
