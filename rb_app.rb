71f20154036d50dece877dc6df736d156ed45240
Rob Bevan <robbevan@xpt.com>
2009-07-01 17:02:56 +0100

    renamed wrapper base > rb_app

---
+++ rb_app.rb
@@ -0,0 +1,35 @@
+optional_templates = [
+  ["jquery",  "Apply jQuery?"],
+  ["blueprint",  "Apply Blueprint?"]
+]
+
+@base_path = if template =~ %r{^(/|\w+://)}
+  File.dirname(template)
+else
+  log '', "You used the app generator with a relative template path."
+  ask "Please enter the full path or URL where the templates are located:"
+end
+
+# init git repo
+git :init
+ 
+def apply_template(t)
+  tmpl = "#{@base_path}/#{t}.rb"
+  log "applying", "template: #{tmpl}"
+  load_template(tmpl)
+  log "applied", tmpl
+  # commit changes
+  git :add => "."
+  git :commit => "-a -m 'applied #{t} app template'"
+end
+
+# first template
+apply_template('first')
+
+# optional templates
+optional_templates.each do |t, q|
+  apply_template(t) if yes?(q)
+end
+
+# last template
+apply_template('last')
