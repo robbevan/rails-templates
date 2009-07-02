01a5f3b3602b9c9ec78ee3bbf89224f61e0d7f68
Rob Bevan <robbevan@xpt.com>
2009-07-02 08:53:33 +0100

    rename README and delete favicon and robots

--- first.rb
+++ first.rb
@@ -2,7 +2,7 @@
 run "rmdir tmp/{ pids, sessions, sockets, cache }"
  
 # remove unnecessary stuff
-run "rm README log/*.log public/index.html public/images/rails.png"
+run "log/*.log public/favicon.ico public/index.html public/images/rails.png public/robots.txt"
  
 # basic .gitignore file
 file '.gitignore', 
@@ -28,3 +28,8 @@ run "cp config/database.yml config/database.yml.sample"
 
 # copy production environment to staging
 run "cp config/environments/production.rb config/environments/staging.rb"
+
+# rename README
+run "mv README README.md"
+
+file 'README.md', "TODO"
