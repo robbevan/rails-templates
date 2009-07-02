fe1cbb36f7a682a5584f52b8f948407513d81e0c
Rob Bevan <robbevan@xpt.com>
2009-07-02 12:29:48 +0100

    updated database.yml to match Rails' default

--- database.rb
+++ database.rb
@@ -2,14 +2,20 @@
 file 'config/database.yml',
 %q{development:
   adapter: mysql
+  encoding: utf8
+  reconnect: false
   database: APP_NAME_development
+  pool: 5
   username: root
   password:
   socket: /opt/local/var/run/mysql5/mysqld.sock
 
 test:
   adapter: mysql
+  encoding: utf8
+  reconnect: false
   database: APP_NAME_test
+  pool: 5
   username: root
   password:
   socket: /opt/local/var/run/mysql5/mysqld.sock
